d604a7931135513ef5acc222c06eace3ef395d7b
Keziah Biermann <development@kizzycode.de>
2022-08-05 22:36:08 +0200

    Fix user generation

--- files/start.sh
+++ files/start.sh
@@ -1,42 +1,22 @@
 #!/bin/sh
-set -eu
+set -euo pipefail
 
 # Create user
 if ! getent passwd "$SMB_USER" >/dev/null; then
-    # Print status
+    # Create user and set password
     echo "Creating user $SMB_USER"
-    
-    # Determine GID for `nogroup`
-    UID=1000
-    GID=`getent group nogroup | cut -d":" -f"3" || echo ""`
-    if test -z "$GID"; then
-        echo "Failed to determine GID for nogroup"
-        exit 1
-    fi
+    adduser -S -H -D -u 1000 -s /sbin/nologin "$SMB_USER"
 
-    # Add the system user to the userdb
-    if test -n "`tail -c 1 /etc/passwd`"; then
-        echo "" >> /etc/passwd
-    fi
-    echo "$SMB_USER:x:$UID:$GID:sambauser:/:/sbin/nologin" >> /etc/passwd
-
-    # Set system user password
-    if test -n "`tail -c 1 /etc/shadow`"; then
-        echo "" >> /etc/shadow
-    fi
-    echo "$SMB_USER:!::0:::::" >> /etc/shadow
+    # Create config
+    export SMB_LOGLEVEL=${SMB_LOGLEVEL:-1}
+    cat /etc/samba/smb.conf.template | envsubst > /etc/samba/smb.conf
 
     # Set the SMB password
     if test -z "$SMB_PASS"; then
         echo "Missing \$SMB_PASS"
         exit 1
     fi
-    smbpasswd -a -n "$SMB_USER" >/dev/null
-    echo -e "$SMB_PASS\n$SMB_PASS" | smbpasswd "$SMB_USER" >/dev/null
-
-    # Create config
-    export SMB_LOGLEVEL=${SMB_LOGLEVEL:-1}
-    cat /etc/samba/smb.conf.template | envsubst > /etc/samba/smb.conf
+    echo -e "$SMB_PASS\n$SMB_PASS" | smbpasswd -L -a -c /etc/samba/smb.conf "$SMB_USER"
 fi
 
 # Start supervisor
